769fa835aec118e4b62935d54c94567af2916d63
ramshell688 <ramonzost68@gmail.com>
2026-04-09 21:51:07 +0200

    annibale: Build AOSP NXP authsecret HAL

--- annibale-vendor.mk
+++ annibale-vendor.mk
@@ -1407,7 +1407,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/annibale/proprietary/odm/etc/dolby/dax-default-spatializer.xml:$(TARGET_COPY_OUT_ODM)/etc/dolby/dax-default-spatializer.xml \
     vendor/xiaomi/annibale/proprietary/odm/etc/dolby/dax-default.xml:$(TARGET_COPY_OUT_ODM)/etc/dolby/dax-default.xml \
     vendor/xiaomi/annibale/proprietary/odm/etc/init.panel_info.sh:$(TARGET_COPY_OUT_ODM)/etc/init.panel_info.sh \
-    vendor/xiaomi/annibale/proprietary/odm/etc/init/android.hardware.authsecret-service.nxp.rc:$(TARGET_COPY_OUT_ODM)/etc/init/android.hardware.authsecret-service.nxp.rc \
     vendor/xiaomi/annibale/proprietary/odm/etc/init/android.hardware.gnss-aidl-service-qti.rc:$(TARGET_COPY_OUT_ODM)/etc/init/android.hardware.gnss-aidl-service-qti.rc \
     vendor/xiaomi/annibale/proprietary/odm/etc/init/android.hardware.security.keymint-service.strongbox.nxp.rc:$(TARGET_COPY_OUT_ODM)/etc/init/android.hardware.security.keymint-service.strongbox.nxp.rc \
     vendor/xiaomi/annibale/proprietary/odm/etc/init/android.hardware.weaver-service.nxp.rc:$(TARGET_COPY_OUT_ODM)/etc/init/android.hardware.weaver-service.nxp.rc \
@@ -3560,7 +3559,6 @@ PRODUCT_PACKAGES += \
     libarcsoft_turbo_hdr_raw \
     libarcsoft_turbo_hdr_raw_portrait \
     libaudioroute_ext \
-    libauthsecret_empty-nxp \
     libauto_focus_stark \
     libbarrage \
     libbitmlengine \
@@ -3911,7 +3909,6 @@ PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.quickcamera.xml \
     vendor.qti.qccsyshal_aidl-service.xml \
     vendor.qti.qesdsys.service.xml \
-    android.hardware.authsecret-service.nxp.xml \
     android.hardware.gnss-aidl-service-qti.xml \
     android.hardware.security.keymint-service.strongbox.xml \
     android.hardware.security.sharedsecret-service.strongbox.xml \
@@ -4061,7 +4058,6 @@ PRODUCT_PACKAGES += \
     xtwifi-client \
     qccsyshal_aidl-service \
     wfdservice64 \
-    android.hardware.authsecret-service.nxp-qti \
     android.hardware.gnss-aidl-service-qti \
     android.hardware.security.keymint-service.strongbox-nxp \
     android.hardware.weaver-service.nxp-qti \
